92b4d5698b2150059bb10b6ea614be329629f955
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-18 19:29:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 holiday_hash.each do |season, holiday|
   puts season.to_s.capitalize + ":" 
-  holiday .each do |holiday_name, supplies|
+  holiday.each do |holiday_name, supplies|
     array=holiday_name.to_s.split("_").map do |word|
       word.capitalize
   end
