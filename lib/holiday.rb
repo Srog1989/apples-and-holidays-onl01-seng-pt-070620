f42e8f2bd843b0b76868fe6c715d432d70757371
Sarah Rogers <s.rogers0715@yahoo.com>
2020-07-18 19:33:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, supplies|
     if supplies.include? "BBQ"
-      bbq_holidays<< holidays
+      bbq_holidays<< holiday
       end
     end
   end
